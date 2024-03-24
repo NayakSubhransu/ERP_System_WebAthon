0721053c7300dc4efcd1ca341ee265b7153c08a3
Divya Kumar <divyakumar768800@gmail.com>
2024-03-24 21:42:33 +0530

    Feedback Admin Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import Feedback from './components/Feedback';
+import FeedbackAdmin from './components/Feedback_Admin';
 function App() {
   return (
     <div className="App">
       <Feedback />
+      <FeedbackAdmin />
     </div>
   );
 }
