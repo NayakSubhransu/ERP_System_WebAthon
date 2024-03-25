8ac7116cc7c4f311780d6ea949f8fe6d2ed1524c
Divya Kumar <divyakumar768800@gmail.com>
2024-03-25 21:08:50 +0530

    Final Commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import Feedback from './components/Feedback';
 import FeedbackAdmin from './components/Feedback_Admin';
 import CourseStudent from './components/CourseStudent';
 import CourseAdmin from './components/CourseAdmin';
+import AcadmicDetails from './components/AcadmicDetail';
+import ContactDetail from './components/ContactDetail';
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,8 @@ function App() {
       <FeedbackAdmin />
       <CourseStudent />
       <CourseAdmin />
+      <AcadmicDetails />
+      <ContactDetail></ContactDetail>
     </div>
   );
 }
